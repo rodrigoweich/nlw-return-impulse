67f601e5695ad17bd00f447d2fc86947c8796678
Rodrigo Weich <rodrigo.weich@ciss.com.br>
2022-05-30 08:55:54 -0300

    Integração WEB x SERVER aula 5

--- web/src/components/WidgetForm/Steps/FeedbackContentStep.tsx
+++ web/src/components/WidgetForm/Steps/FeedbackContentStep.tsx
@@ -1,7 +1,9 @@
 import { ArrowLeft, Camera } from "phosphor-react";
 import { FormEvent, useState } from "react";
 import { FeedbackType, feedbackTypes } from "..";
+import { api } from "../../../lib/api";
 import { CloseButton } from "../../CloseButton";
+import { Loading } from "../../Loading";
 import { ScreenshotButton } from "../ScreenshotButton";
 
 interface FeedbackContentStepProps {
@@ -14,13 +16,17 @@ export function FeedbackContentStep({ feedbackType, onFeedbackRestartRequested, 
   const feedbackTypeInfo = feedbackTypes[feedbackType];
   const [screenshot, setScreenshot] = useState<string | null>(null);
   const [comment, setComment] = useState('');
+  const [isSendingFeedback, setIsSendingFeedback] = useState(false);
 
-  function handleSubmitFeedback(event: FormEvent) {
+  async function handleSubmitFeedback(event: FormEvent) {
     event.preventDefault();
-    console.log({
+    setIsSendingFeedback(true);
+    await api.post('/feedbacks', {
+      type: feedbackType,
+      comment,
       screenshot,
-      comment
     })
+    setIsSendingFeedback(false);
     onFeedbackSent();
   }
 
@@ -40,7 +46,7 @@ export function FeedbackContentStep({ feedbackType, onFeedbackRestartRequested, 
         <textarea className=" min-w-[304px] w-full min-h-[112px] text-sm placeholder-zinc-400 text-zinc-100 border-zinc-600 bg-transparent rounded-md focus:border-brand-500 focus:ring-brand-500 focus:ring-1 resize-none focus:outline-none scrollbar scrollbar-thumb-zinc-700 scrollbar-track-transparent scrollbar-thin" placeholder="Conte com detalhes o que está acontecendo..." onChange={event => setComment(event.target.value)}></textarea>
         <footer className="flex gap-2 mt-2">
           <ScreenshotButton screenshot={screenshot} onScreenshotTook={setScreenshot} />
-          <button type="submit" disabled={comment.length === 0} className="p-2 bg-brand-500 rounded-[4px] border-transparent flex-1 flex justify-center items-center text-sm hover:bg-brand-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500 transition-colors disabled:opacity-50 disabled:hover:bg-brand-500">Enviar feedback</button>
+          <button type="submit" disabled={comment.length === 0 || isSendingFeedback} className="p-2 bg-brand-500 rounded-[4px] border-transparent flex-1 flex justify-center items-center text-sm hover:bg-brand-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500 transition-colors disabled:opacity-50 disabled:hover:bg-brand-500">{isSendingFeedback ? <Loading /> : 'Enviar feedback'}</button>
         </footer>
       </form>
     </>
